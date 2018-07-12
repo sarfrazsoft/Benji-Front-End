5f9a93cd474bac1de674eabf99f7cfd2bdad1c83
OPT_DOMAIN\mohiuda <amohiuddin@optrust.com>
2018-07-12 09:35:03 -0400

    Removing unneeded files

--- src/app/activity.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-  selector: '[appActivity]',
-})
-export class ActivityDirective {
-  constructor(public viewContainerRef: ViewContainerRef) { }
-}
