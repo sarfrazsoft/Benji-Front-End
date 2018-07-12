819c52ecb3ad8efbb22d73f07f95de01398053e1
OPT_DOMAIN\mohiuda <amohiuddin@optrust.com>
2018-07-12 08:48:30 -0400

    FOR DEMO PURPOSES: Frontend will now create a new courserun and sessionrun for that course run. Allows isolation of multiple started sessions.

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -18,7 +18,7 @@ export class LandingComponent implements OnInit {
 
   loginAndStart() {
     this.auth.login('admin', 'benji112').subscribe(
-      response => this.backend.get_runnable_sessions(1).subscribe(
+      response => this.backend.create_courserun(1).subscribe(
                   resp => this.checkAndStart(resp),
                   err => console.log(err)
       ),
@@ -26,14 +26,10 @@ export class LandingComponent implements OnInit {
     );
   }
 
-  checkAndStart(runnable_sessions) {
-    if (runnable_sessions.length < 1) {
-      console.log('Error: No runnable sessions found');
-    } else {
-      this.backend.start_session(1, runnable_sessions[0].session_num).subscribe(
-        resp => this.router.navigate(['/desktop/session', {sessionRunID: resp['id']}]),
-        err => console.log(err)
-      );
-    }
+  checkAndStart(courserun) {
+    this.backend.start_next_session(courserun.id).subscribe(
+      resp => this.router.navigate(['/desktop/session', {sessionRunID: resp['id']}]),
+      err => console.log(err)
+    );
   }
 }
