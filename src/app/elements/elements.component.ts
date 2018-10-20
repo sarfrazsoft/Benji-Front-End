2b60f12431376c6155e04a8504c666057b544103
Andrew Thompson <atthompson92@gmail.com>
2018-10-20 12:01:08 -0400

    Participant elements

---
+++ src/app/elements/elements.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, TemplateRef, ElementRef, ViewChild, Inject } from '@angular/core';
+import {MatDialog} from '@angular/material';
+
+declare var twemoji: any;
+declare var particlesJS: any;
+
+
+@Component({
+  selector: 'app-elements',
+  templateUrl: './elements.component.html',
+  styleUrls: ['./elements.component.scss']
+})
+export class ElementsComponent implements OnInit {
+
+  constructor(public dialog: MatDialog) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  public triggerDialogue(templateRef) {
+    this.dialog.open(templateRef, {
+      width: '305px',
+      panelClass: 'dialog--indigo-blue'
+    });
+  }
+
+
+
+}
