8a43e981e0c862391bc1b2ff386bb84850a4cc5f
Abdullah Mohiuddin <a.nidal@gmail.com>
2018-10-28 11:57:46 -0400

    Added logic for participant screens: lobby, discussion, teletrivia

--- src/app/pages/participant/lesson/participant-discussion-activity/participant-discussion-activity.component.ts
+++ src/app/pages/participant/lesson/participant-discussion-activity/participant-discussion-activity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-participant-discussion-activity',
@@ -6,11 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./participant-discussion-activity.component.scss']
 })
 export class ParticipantDiscussionActivityComponent implements OnInit {
+  @Input()
+  set socketData(data) {
+    const activity = data.message.activity_status;
+
+    this.sharingStarted = activity.sharer_countdown_time.length === 0;
+
+    if (this.sharingStarted) {
+      const currentGroup = activity.selected_sharers[activity.sharer_group_num];
+      this.iAmSharing = data.message.your_identity.id === currentGroup.primary || data.message.your_identity.id === currentGroup.secondary;
+    }
+  }
+
+  @Output()
+  socketMessage = new EventEmitter<any>();
+
+  public sharingStarted;
+  public iAmSharing;
 
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  shareButton() {
+    const message = {'event': 'share_button'};
+    this.socketMessage.emit(message);
+  }
+
+  doneButton() {
+    const message = {'event': 'done_button'};
+    this.socketMessage.emit(message);
   }
 
 }
--- src/app/pages/participant/lesson/participant-lobby/participant-lobby.component.ts
+++ src/app/pages/participant/lesson/participant-lobby/participant-lobby.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-participant-lobby',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./participant-lobby.component.scss']
 })
 export class ParticipantLobbyComponent implements OnInit {
+
+  @Input()
+  set socketData(data) {
+    this.title = data.message.lesson.lesson_name;
+  }
+
+  public title;
 
   constructor() { }
 
