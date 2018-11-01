eae35935f964c35386c189d206e55c12dc39f80f
Abdullah Mohiuddin <a.nidal@gmail.com>
2018-10-31 22:14:23 -0400

    Adding logic for getting roles and partner information from websocket frames in pair activity

--- src/app/pages/participant/lesson/participant-pair-activity/participant-pair-activity.component.ts
+++ src/app/pages/participant/lesson/participant-pair-activity/participant-pair-activity.component.ts
@@ -13,15 +13,36 @@ export class ParticipantPairActivityComponent implements OnInit {
     const activity = data.message.activity_status;
 
     this.pairActivityStarted = activity.all_pairs_found;
-    this.isReady = activity.user_pairs_found.indexOf(data.message.your_identity.id) > 0;
+    this.isReady = activity.user_pairs_found.indexOf(data.message.your_identity.id) > 0
 
-    for (let group of activity.user_groups) {
+    if (!this.timeSet) {
+      this.timeRemaining = activity.activity_timer;
+      this.timeSet = true;
+    }
+
+    let partnerID;
+
+    for (const group of activity.user_groups) {
       if (group.primary.indexOf(data.message.your_identity.id) > 0) {
         this.roleEmoji = this.emoji.getEmoji('zipface'); // TODO WHAT IS THE EMOJI FOR THE SEPAKER?
+        partnerID = group.secondary[0];
+        this.roleName = activity.primary_role.role_name;
+        this.roleInstructions = activity.primary_role.role_instructions;
       } else if (group.secondary.indexOf(data.message.your_identity.id) > 0) {
         this.roleEmoji = this.emoji.getEmoji('zipface');
+        partnerID = group.primary[0];
+        this.roleName = activity.secondary_role.role_name;
+        this.roleInstructions = activity.secondary_role.role_instructions;
       }
     }
+
+    for (const part of data.message.participants) {
+      if (part.id === partnerID) {
+        this.partnerName = part.first_name;
+      }
+    }
+
+
   }
 
   @Output()
@@ -32,6 +53,11 @@ export class ParticipantPairActivityComponent implements OnInit {
   public isReady = false;
   public pairActivityStarted = true;
   public roleEmoji: string;
+  public partnerName = '';
+  public roleName = '';
+  public roleInstructions = '';
+  public timeRemaining = 60;
+  public timeSet = false;
 
   ngOnInit() {
     this.roleEmoji = this.emoji.getEmoji('zipface');
