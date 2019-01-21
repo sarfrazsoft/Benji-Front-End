36dacf158c84a00f235b92fb7c70d4a689a1de00
matemahinkhan <tutenstine@gmail.com>
2019-01-21 13:48:48 -0500

    feat(ms-toolbar): added fullscreen and exit fullscreen icon

--- src/app/ui-components/main-screen-toolbar/main-screen-toolbar.component.ts
+++ src/app/ui-components/main-screen-toolbar/main-screen-toolbar.component.ts
@@ -5,11 +5,51 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './main-screen-toolbar.component.html',
   styleUrls: ['./main-screen-toolbar.component.scss']
 })
-export class MainScreenToolbarComponent implements OnInit {
+export class MainScreenToolbarComponent {
+  isFullscreen = false;
 
-  constructor() { }
+  openfullscreen() {
+    // Trigger fullscreen
+    const docElmWithBrowsersFullScreenFunctions = document.documentElement as HTMLElement & {
+      mozRequestFullScreen(): Promise<void>;
+      webkitRequestFullscreen(): Promise<void>;
+      msRequestFullscreen(): Promise<void>;
+    };
 
-  ngOnInit() {
+    if (docElmWithBrowsersFullScreenFunctions.requestFullscreen) {
+      docElmWithBrowsersFullScreenFunctions.requestFullscreen();
+    } else if (docElmWithBrowsersFullScreenFunctions.mozRequestFullScreen) {
+      /* Firefox */
+      docElmWithBrowsersFullScreenFunctions.mozRequestFullScreen();
+    } else if (docElmWithBrowsersFullScreenFunctions.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      docElmWithBrowsersFullScreenFunctions.webkitRequestFullscreen();
+    } else if (docElmWithBrowsersFullScreenFunctions.msRequestFullscreen) {
+      /* IE/Edge */
+      docElmWithBrowsersFullScreenFunctions.msRequestFullscreen();
+    }
+    this.isFullscreen = true;
   }
 
+  closefullscreen() {
+    const docWithBrowsersExitFunctions = document as Document & {
+      mozCancelFullScreen(): Promise<void>;
+      webkitExitFullscreen(): Promise<void>;
+      msExitFullscreen(): Promise<void>;
+    };
+
+    if (docWithBrowsersExitFunctions.exitFullscreen) {
+      docWithBrowsersExitFunctions.exitFullscreen();
+    } else if (docWithBrowsersExitFunctions.mozCancelFullScreen) {
+      /* Firefox */
+      docWithBrowsersExitFunctions.mozCancelFullScreen();
+    } else if (docWithBrowsersExitFunctions.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      docWithBrowsersExitFunctions.webkitExitFullscreen();
+    } else if (docWithBrowsersExitFunctions.msExitFullscreen) {
+      /* IE/Edge */
+      docWithBrowsersExitFunctions.msExitFullscreen();
+    }
+    this.isFullscreen = false;
+  }
 }
