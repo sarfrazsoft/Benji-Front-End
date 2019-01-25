76f927a8982286b25f7556c44d9cdd1f0bee0bd3
matemahinkhan <tutenstine@gmail.com>
2019-01-25 10:44:21 -0500

    build: setup environment variables for prod and local builds

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,6 +1,8 @@
 'use strict';
 
-const backend = 'localhost';
+import { environment } from './../environments/environment';
+
+const backend = environment.host;
 
 export const apiRoot = 'http://' + backend + ':8000';
 export const wsRoot = 'ws://' + backend + ':8000';
