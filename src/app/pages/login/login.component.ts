cf0358ce31153e28a9eb4d50db0fc084e89e741c
Muhammad Mahin Khan <tutenstine@gmail.com>
2019-02-06 17:58:16 -0500

    feat(admin): added login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'benji-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private builder: FormBuilder) {}
 
   ngOnInit() {
+    this.form = this.builder.group({
+      email: '',
+      password: ''
+    });
   }
 
+  onSubmit() {
+    if (this.form.valid) {
+    }
+  }
 }
