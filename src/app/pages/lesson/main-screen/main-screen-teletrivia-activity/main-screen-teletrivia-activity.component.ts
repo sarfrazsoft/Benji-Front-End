73cf9f9353c05db8266a5ce88995b52c5c2b6958
Muhammad Mahin Khan <tutenstine@gmail.com>
2019-02-27 10:36:36 -0500

    feat(ms-teletrivia): show only top 8 sorted on leaderboard

--- src/app/pages/lesson/main-screen/main-screen-teletrivia-activity/main-screen-teletrivia-activity.component.ts
+++ src/app/pages/lesson/main-screen/main-screen-teletrivia-activity/main-screen-teletrivia-activity.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
+import { orderBy, sortBy } from 'lodash';
 import { BaseActivityComponent } from '../../shared/base-activity.component';
 
 @Component({
@@ -6,12 +7,21 @@ import { BaseActivityComponent } from '../../shared/base-activity.component';
   templateUrl: './main-screen-teletrivia-activity.component.html',
   styleUrls: ['./main-screen-teletrivia-activity.component.scss']
 })
-export class MainScreenTeletriviaActivityComponent extends BaseActivityComponent {
+export class MainScreenTeletriviaActivityComponent extends BaseActivityComponent
+  implements OnChanges {
   timerInit(timer) {
     const totalTime =
       Date.parse(this.activityState.activity_status.circle_countdown) -
       Date.now();
     const circleTimerSecondsElapsed = 0;
     timer.startTimer(totalTime, circleTimerSecondsElapsed);
+  }
+
+  ngOnChanges() {
+    this.activityState.activity_status.leaderboard = orderBy(
+      this.activityState.activity_status.leaderboard,
+      ['correct'],
+      ['desc']
+    );
   }
 }
