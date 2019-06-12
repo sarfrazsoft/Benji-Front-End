7f04ea580d4f0b26523caeb5da07c4036ee0efd8
Muhammad Mahin Khan <tutenstine@gmail.com>
2019-06-12 14:47:15 -0400

    feat(global): changed backend port to 8080

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -4,5 +4,5 @@ import { environment } from './../environments/environment';
 
 const backend = environment.host;
 
-export const apiRoot = 'http://' + backend + ':8000';
-export const wsRoot = 'ws://' + backend + ':8000';
+export const apiRoot = 'http://' + backend + ':8080';
+export const wsRoot = 'ws://' + backend + ':8080';
