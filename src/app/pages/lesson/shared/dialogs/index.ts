71f872de1b0d9113571394097c84ff4ecc08a065
Muhammad Mahin Khan <tutenstine@gmail.com>
2019-08-27 10:33:28 -0400

    feat(low-attendance): low attendance dialog added

--- src/app/pages/lesson/shared/dialogs/index.ts
+++ src/app/pages/lesson/shared/dialogs/index.ts
@@ -1,5 +1,10 @@
 export * from './low-response/low-response.dialog';
+export * from './low-attendance/low-attendance.dialog';
 
+import { LowAttendanceDialogComponent } from './low-attendance/low-attendance.dialog';
 import { LowResponseDialogComponent } from './low-response/low-response.dialog';
 
-export const LessonDialogs = [LowResponseDialogComponent];
+export const LessonDialogs = [
+  LowResponseDialogComponent,
+  LowAttendanceDialogComponent
+];
