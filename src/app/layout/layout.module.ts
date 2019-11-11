6759ff5517023b46bd5b3a186bbfa353b89c1772
Muhammad Mahin Khan <tutenstine@gmail.com>
2019-11-11 12:14:35 -0500

    fix(modules): remove videogular modules

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -8,24 +8,13 @@ import { SharedModule } from './../shared/shared.module';
 import { EntryComponents, LayoutComponent, LayoutDeclarations } from './';
 import { LayoutRoutes } from './layout.routing';
 
-import { VgBufferingModule } from 'videogular2/buffering';
-import { VgControlsModule } from 'videogular2/controls';
-import { VgCoreModule } from 'videogular2/core';
-import { VgOverlayPlayModule } from 'videogular2/overlay-play';
-import { VgStreamingModule } from 'videogular2/streaming';
-
 @NgModule({
   imports: [
     CommonModule,
     LayoutRoutes,
     FormsModule,
     ReactiveFormsModule,
-    SharedModule,
-    VgCoreModule,
-    VgControlsModule,
-    VgOverlayPlayModule,
-    VgBufferingModule,
-    VgStreamingModule
+    SharedModule
   ],
   exports: [RouterModule, LayoutComponent],
   declarations: [...LayoutDeclarations],
