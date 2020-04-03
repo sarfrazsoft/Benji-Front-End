de0e6d7457a5fc7a59dfad9195a290abca2e3611
Muhammad Mahin Khan <tutenstine@gmail.com>
2020-04-03 14:41:17 +0500

    feat(custom-tooltip): commented out code for custom tooltip

--- src/app/shared/components/feedback-graph/question/question.component.ts
+++ src/app/shared/components/feedback-graph/question/question.component.ts
@@ -82,6 +82,69 @@ export class QuestionComponent implements OnInit, AfterViewInit {
 
     // console.log(max);
 
+    // Chart.defaults.global.tooltips.custom = function(tooltip) {
+    //   // Tooltip Element
+    //   const tooltipEl = document.getElementById('chartjs-tooltip');
+
+    //   // Hide if no tooltip
+    //   if (tooltip.opacity === 0) {
+    //     tooltipEl.style.opacity = '0';
+    //     return;
+    //   }
+
+    //   // Set caret Position
+    //   tooltipEl.classList.remove('above', 'below', 'no-transform');
+    //   if (tooltip.yAlign) {
+    //     tooltipEl.classList.add(tooltip.yAlign);
+    //   } else {
+    //     tooltipEl.classList.add('no-transform');
+    //   }
+
+    //   function getBody(bodyItem) {
+    //     return bodyItem.lines;
+    //   }
+
+    //   // Set Text
+    //   if (tooltip.body) {
+    //     const titleLines = tooltip.title || [];
+    //     const bodyLines = tooltip.body.map(getBody);
+
+    //     let innerHtml = '<thead>';
+
+    //     titleLines.forEach(function(title) {
+    //       innerHtml += '<tr><th>' + title + '</th></tr>';
+    //     });
+    //     innerHtml += '</thead><tbody>';
+
+    //     bodyLines.forEach(function(body, i) {
+    //       const colors = tooltip.labelColors[i];
+    //       let style = 'background:' + colors.backgroundColor;
+    //       style += '; border-color:' + colors.borderColor;
+    //       style += '; border-width: 2px';
+    //       const span =
+    //         '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+    //       innerHtml += '<tr><td>' + span + body + '</td></tr>';
+    //     });
+    //     innerHtml += '</tbody>';
+
+    //     const tableRoot = tooltipEl.querySelector('table');
+    //     tableRoot.innerHTML = innerHtml;
+    //   }
+
+    //   const positionY = this._chart.canvas.offsetTop;
+    //   const positionX = this._chart.canvas.offsetLeft;
+
+    //   // Display, position, and set styles for font
+    //   tooltipEl.style.opacity = '1';
+    //   tooltipEl.style.left = positionX + tooltip.caretX + 'px';
+    //   tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+    //   tooltipEl.style.fontFamily = tooltip._bodyFontFamily;
+    //   tooltipEl.style.fontSize = tooltip.bodyFontSize + '';
+    //   tooltipEl.style.fontStyle = tooltip._bodyFontStyle;
+    //   tooltipEl.style.padding =
+    //     tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+    // };
+
     this.canvas = document.getElementById('myChart');
     this.ctx = this.chartCanvas.nativeElement.getContext('2d');
     this.myChart = new Chart(this.ctx, {
@@ -117,6 +180,20 @@ export class QuestionComponent implements OnInit, AfterViewInit {
             },
             afterBody: (tooltipItems, d) => {
               return this.users[tooltipItems[0].index];
+              // return [
+              //   'yolo',
+              //   'more yolo',
+              //   'yolo',
+              //   'more yolo',
+              //   'yolo',
+              //   'more yolo',
+              //   'yolo',
+              //   'more yolo',
+              //   'yolo',
+              //   'more yolo',
+              //   'yolo',
+              //   'more yolo'
+              // ];
             }
           }
         },
