3acdc79f4c942a892ee758068defdb47b700544f
Muhammad Mahin Khan <tutenstine@gmail.com>
2020-08-27 10:07:27 -0400

    feat(past-sessions): show appropriate name on past session key stats and refactor

--- src/app/dashboard/past-sessions/reports/key-stats/key-stats.component.ts
+++ src/app/dashboard/past-sessions/reports/key-stats/key-stats.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnInit
-} from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { ContextService, PastSessionsService } from 'src/app/services';
 import { ActivityReport } from 'src/app/services/backend/schema';
@@ -12,23 +6,21 @@ import { ActivityReport } from 'src/app/services/backend/schema';
 @Component({
   selector: 'benji-key-stats',
   templateUrl: './key-stats.component.html',
-  styleUrls: ['./key-stats.component.scss']
+  styleUrls: ['./key-stats.component.scss'],
 })
 export class KeyStatsComponent implements OnInit, OnChanges {
   @Input() data: ActivityReport;
   userIsAdmin;
+  lessonName = '';
   startDate = '';
   startTime = '';
   endDate = '';
   hostName = '';
   duration;
-  constructor(
-    private pastSessionsService: PastSessionsService,
-    private contextService: ContextService
-  ) {}
+  constructor(private pastSessionsService: PastSessionsService, private contextService: ContextService) {}
 
   ngOnInit() {
-    this.contextService.user$.subscribe(user => {
+    this.contextService.user$.subscribe((user) => {
       if (user.local_admin_permission) {
         this.userIsAdmin = true;
       }
@@ -36,23 +28,22 @@ export class KeyStatsComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {
-    if (this.data && this.data.end_time) {
-      this.duration = moment.duration(
-        moment(this.data.end_time).diff(this.data.start_time)
-      ) as moment.Duration;
+    if (this.data) {
+      this.lessonName = this.data.lesson.lesson_name;
+      if (this.data.end_time) {
+        this.duration = moment.duration(
+          moment(this.data.end_time).diff(this.data.start_time)
+        ) as moment.Duration;
+        this.duration =
+          this.duration.hours() +
+          'h ' +
+          (this.duration.minutes() < 9 ? '0' + this.duration.minutes() : this.duration.minutes()) +
+          'm';
+      }
       this.startDate = moment(this.data.start_time).format('MMM D, YYYY');
       this.startTime = moment(this.data.start_time).format('hh:mma');
 
-      this.duration =
-        this.duration.hours() +
-        'h ' +
-        (this.duration.minutes() < 9
-          ? '0' + this.duration.minutes()
-          : this.duration.minutes()) +
-        'm';
-
-      this.hostName =
-        this.data.host.first_name + ' ' + this.data.host.last_name;
+      this.hostName = this.data.host.first_name + ' ' + this.data.host.last_name;
     }
   }
 
--- src/app/dashboard/past-sessions/reports/reports.component.ts
+++ src/app/dashboard/past-sessions/reports/reports.component.ts
@@ -48,68 +48,66 @@ export class ReportsComponent implements OnInit {
   }
 
   getSessionSummary(lessonrunCode: string) {
-    this.pastSessionsService
-      .getReports(lessonrunCode)
-      .subscribe((res: Array<ActivityReport>) => {
-        this.statsData = res[0];
-        // Iterate over each item in array
-        res.forEach((act: ActivityReport) => {
-          if (act.activity_type === Acts.mcq) {
-            const mcqCF = this.cfr.resolveComponentFactory(McqsComponent);
-            const component = this.entry.createComponent(mcqCF);
-            component.instance.data = act;
+    this.pastSessionsService.getReports(lessonrunCode).subscribe((res: Array<ActivityReport>) => {
+      this.statsData = res[0];
+      // Iterate over each item in array
+      res.forEach((act: ActivityReport) => {
+        if (act.activity_type === Acts.mcq) {
+          const mcqCF = this.cfr.resolveComponentFactory(McqsComponent);
+          const component = this.entry.createComponent(mcqCF);
+          component.instance.data = act;
 
-            // commented out tags feedback report
-            // const FTagsCF = this.cfr.resolveComponentFactory(
-            //   FeedbackTagsComponent
-            // );
-            // const component = this.entry.createComponent(FTagsCF);
-            // component.instance.data = [
-            //   {
-            //     question: 'how do evaluate the pitch',
-            //     tagScores: [
-            //       { name: 'Nice', score: 6 },
-            //       { name: 'Cool', score: 4 },
-            //       { name: 'Bad', score: 2 },
-            //       { name: 'The worse', score: 2 }
-            //     ]
-            //   },
-            //   {
-            //     question: 'how to chase a laser beam',
-            //     tagScores: [
-            //       { name: 'fast', score: 7 },
-            //       { name: 'Nyoom', score: 5 },
-            //       { name: 'slow', score: 2 },
-            //       { name: 'creeping', score: 4 }
-            //     ]
-            //   }
-            // ];
-          } else if (act.activity_type === Acts.feedback) {
-            const f = this.cfr.resolveComponentFactory(FeedbackComponent);
-            const component = this.entry.createComponent(f);
-            component.instance.data = act;
-          } else if (act.activity_type === Acts.pitchoMatic) {
-            const p = this.cfr.resolveComponentFactory(PitchOMaticComponent);
-            const component = this.entry.createComponent(p);
-            component.instance.data = act;
-          } else if (act.activity_type === Acts.buildAPitch) {
-            const b = this.cfr.resolveComponentFactory(BuildAPitchComponent);
-            const component = this.entry.createComponent(b);
-            component.instance.data = act;
-          } else if (act.activity_type === Acts.genericRoleplay) {
-            // const g = this.cfr.resolveComponentFactory(GenericRoleplayComponent);
-            // const component = this.entry.createComponent(g);
-            // component.instance.data = act;
-          } else if (act.activity_type === Acts.brainStorm) {
-            const b = this.cfr.resolveComponentFactory(BrainStormComponent);
-            const component = this.entry.createComponent(b);
-            component.instance.data = act;
-          } else if (act.activity_type === Acts.caseStudy) {
-            const c = this.cfr.resolveComponentFactory(CaseStudyComponent);
-            const component = this.entry.createComponent(c);
-            component.instance.data = act;
-          }
-        });
+          // commented out tags feedback report
+          // const FTagsCF = this.cfr.resolveComponentFactory(
+          //   FeedbackTagsComponent
+          // );
+          // const component = this.entry.createComponent(FTagsCF);
+          // component.instance.data = [
+          //   {
+          //     question: 'how do evaluate the pitch',
+          //     tagScores: [
+          //       { name: 'Nice', score: 6 },
+          //       { name: 'Cool', score: 4 },
+          //       { name: 'Bad', score: 2 },
+          //       { name: 'The worse', score: 2 }
+          //     ]
+          //   },
+          //   {
+          //     question: 'how to chase a laser beam',
+          //     tagScores: [
+          //       { name: 'fast', score: 7 },
+          //       { name: 'Nyoom', score: 5 },
+          //       { name: 'slow', score: 2 },
+          //       { name: 'creeping', score: 4 }
+          //     ]
+          //   }
+          // ];
+        } else if (act.activity_type === Acts.feedback) {
+          const f = this.cfr.resolveComponentFactory(FeedbackComponent);
+          const component = this.entry.createComponent(f);
+          component.instance.data = act;
+        } else if (act.activity_type === Acts.pitchoMatic) {
+          const p = this.cfr.resolveComponentFactory(PitchOMaticComponent);
+          const component = this.entry.createComponent(p);
+          component.instance.data = act;
+        } else if (act.activity_type === Acts.buildAPitch) {
+          const b = this.cfr.resolveComponentFactory(BuildAPitchComponent);
+          const component = this.entry.createComponent(b);
+          component.instance.data = act;
+        } else if (act.activity_type === Acts.genericRoleplay) {
+          // const g = this.cfr.resolveComponentFactory(GenericRoleplayComponent);
+          // const component = this.entry.createComponent(g);
+          // component.instance.data = act;
+        } else if (act.activity_type === Acts.brainStorm) {
+          const b = this.cfr.resolveComponentFactory(BrainStormComponent);
+          const component = this.entry.createComponent(b);
+          component.instance.data = act;
+        } else if (act.activity_type === Acts.caseStudy) {
+          const c = this.cfr.resolveComponentFactory(CaseStudyComponent);
+          const component = this.entry.createComponent(c);
+          component.instance.data = act;
+        }
       });
+    });
   }
 }
