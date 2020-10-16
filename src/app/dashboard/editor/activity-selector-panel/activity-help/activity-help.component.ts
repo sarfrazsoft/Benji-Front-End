52c5d4b165aa10e0a44cf95aa4da89661d257b0d
Muhammad Mahin Khan <tutenstine@gmail.com>
2020-10-16 16:38:09 -0400

    feat: activity help

--- src/app/dashboard/editor/activity-selector-panel/activity-help/activity-help.component.ts
+++ src/app/dashboard/editor/activity-selector-panel/activity-help/activity-help.component.ts
@@ -6,9 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity-help.component.scss'],
 })
 export class ActivityHelpComponent implements OnInit {
+  activityHelp = ActivityHelp;
   constructor() {}
 
   ngOnInit() {}
 }
 
-export const TitleActivity = [];
+export const ActivityHelp = [
+  {
+    type: 'header',
+    content: 'Overview',
+  },
+  {
+    type: 'paragraph',
+    content: `Our multiple choice activity works just as you’d expect.
+              Ask your participants a question and they have
+              to pick the correct answer. You select which questions are right.`,
+  },
+  {
+    type: 'paragraph',
+    content: `If you pick multiple correct answers then
+              they have to pick all that are correct. Here’s a video walking
+              you through this!`,
+  },
+  {
+    type: 'image',
+    content: '/assets/img/helpimg.png',
+  },
+  {
+    type: 'header',
+    content: 'Tips',
+  },
+];
