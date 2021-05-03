91a85d1781a9c5e1d4b176043bc8e9918584f634
mahin@mybenji.com <tutenstine@gmail.com>
2021-05-03 19:07:39 -0400

    feat: added e2e test for joining participants

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,28 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementHelper } from 'protractor';
 
 export class AppPage {
   navigateTo() {
+    // return browser.get('https://app.mybenji.com');
     return browser.get('/');
   }
 
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  getWelcomeText() {
+    return element(by.id('hurr')).getText();
+  }
+
+  injectRoomCode(e: ElementHelper, roomCode) {
+    return e(by.id('room-code')).sendKeys(roomCode);
+  }
+
+  injectparticipantName(e: ElementHelper, name: string) {
+    return e(by.id('participant-name')).sendKeys(name);
+  }
+
+  clickEnter(e: ElementHelper) {
+    return e(by.tagName('button')).click();
   }
 }
