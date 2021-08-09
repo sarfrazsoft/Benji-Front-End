9f09bff12f2f259dc90f566b08bff04ec319e6b1
Muhammad Sarfraz <replysarfraz@gmail.com>
2021-08-10 02:27:39 +0500

    Feat: Add sharing timer to sharing screen on Madlibs only.

--- src/app/pages/lesson/main-screen/shared/sharing-tool/build-a-pitch/build-a-pitch.component.ts
+++ src/app/pages/lesson/main-screen/shared/sharing-tool/build-a-pitch/build-a-pitch.component.ts
@@ -1,24 +1,78 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Subject, interval } from 'rxjs';
 import { BuildAPitchService } from 'src/app/services/activities';
 import { UpdateMessage } from 'src/app/services/backend/schema';
 import { Participant } from 'src/app/services/backend/schema/course_details';
-
+export interface Entry {
+  created: Date;
+}
+export interface TimeSpan {
+  minutes: number;
+  seconds: number;
+}
 @Component({
   selector: 'benji-build-a-pitch',
   templateUrl: './build-a-pitch.component.html',
   styleUrls: ['./build-a-pitch.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BuildAPitchComponent implements OnInit, OnChanges {
   @Input() data: UpdateMessage;
   @Input() currentSpeaker: { displayName: string; id: number };
   pitchText = '';
-  constructor(private buildAPitchService: BuildAPitchService) {}
+
+  constructor(private buildAPitchService: BuildAPitchService, private changeDetector: ChangeDetectorRef) {}
+
+  entries: Entry[] = [];
+
+  private destroyed$ = new Subject();
 
   ngOnInit(): void {
     this.getPitchText();
+
+    this.addEntry();
+    
+    interval(1000).subscribe(() => {
+      if (!this.changeDetector['destroyed']) {
+        this.changeDetector.detectChanges();
+      }
+    });
+    this.changeDetector.detectChanges();
   }
 
-  ngOnChanges() {}
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+
+  addEntry() {
+    this.entries.push({
+      created: new Date(),
+    });
+  }
+
+  getElapsedTime(): TimeSpan {  
+    let entry = this.entries[this.entries.length-1];
+    let totalSeconds = Math.floor((new Date().getTime() - entry.created.getTime()) / 1000);
+    
+    let minutes = 0;
+    let seconds = 0;
+
+    if (totalSeconds >= 60) {
+      minutes = Math.floor(totalSeconds / 60);
+      totalSeconds -= 60 * minutes;
+    }
+
+    seconds = totalSeconds;
+    
+    return {
+      minutes: minutes,
+      seconds: seconds
+    };
+  }
+
+  ngOnChanges() {
+  }
 
   update() {
     this.getPitchText();
@@ -30,11 +84,12 @@ export class BuildAPitchComponent implements OnInit, OnChanges {
       this.data.buildapitchactivity,
       true
     );
-
-    console.log(this.data.buildapitchactivity);
+    //console.log(this.data.buildapitchactivity);
+    this.addEntry();
   }
 
   getSharingUserName() {
     return this.currentSpeaker.displayName;
   }
+
 }
