f6758659ea43d00867c53df8957932cf45a4926e
Muhammad Sarfraz <replysarfraz@gmail.com>
2021-11-09 08:52:30 +0500

    Feat: ImportSlidesDialogComponent commented out in index.ts

--- src/app/shared/dialogs/index.ts
+++ src/app/shared/dialogs/index.ts
@@ -10,17 +10,18 @@ export * from './templates-dialog/templates.dialog';
 export * from './idea-creation-dialog/idea-creation.dialog';
 export * from './grouping-tool-dialog/grouping-tool.dialog';
 export * from './idea-detailed-dialog/idea-detailed.dialog';
+export * from './import-slides-dialog/import-slides.dialog'
 export * from './participant-grouping/participant-grouping.dialog';
 
 import { AddVideoDialogComponent } from './add-video-dialog/add-video.dialog';
 import { ConfirmationDialogComponent } from './confirmation/confirmation.dialog';
 import { ErrorMessageDialogComponent } from './error-message/error-message.dialog';
-import { ParticipantGroupingDialogComponent } from './participant-grouping/participant-grouping.dialog';
 import { GroupingToolDialogComponent } from './grouping-tool-dialog/grouping-tool.dialog';
 import { IdeaCreationDialogComponent } from './idea-creation-dialog/idea-creation.dialog';
 import { IdeaDetailedDialogComponent } from './idea-detailed-dialog/idea-detailed.dialog';
 import { GiphyComponent } from './image-picker-dialog/giphy/giphy.component';
 import { ImagePickerDialogComponent } from './image-picker-dialog/image-picker.dialog';
+import { ImportSlidesDialogComponent } from './import-slides-dialog/import-slides.dialog'
 import { UnsplashComponent } from './image-picker-dialog/unsplash/unsplash.component';
 import { UploadImageComponent } from './image-picker-dialog/upload-image/upload-image.component';
 import { JobInfoDialogComponent } from './job-info/job-info.dialog';
@@ -28,6 +29,7 @@ import { JoinSessionDialogComponent } from './join-session/join-session.dialog';
 import { LaunchSessionDialogComponent } from './launch-session/launch-session.dialog';
 import { LessonSettingsDialogComponent } from './lesson-settings-dialog/lesson-settings.dialog';
 import { MCQFeedbackDialogComponent } from './mcq-feedback/mcq-feedback.dialog';
+import { ParticipantGroupingDialogComponent } from './participant-grouping/participant-grouping.dialog';
 import { TemplatesDialogComponent } from './templates-dialog/templates.dialog';
 
 export const Dialogs = [
@@ -35,6 +37,7 @@ export const Dialogs = [
   ConfirmationDialogComponent,
   ErrorMessageDialogComponent,
   ImagePickerDialogComponent,
+ // ImportSlidesDialogComponent,
   UnsplashComponent,
   GiphyComponent,
   UploadImageComponent,
