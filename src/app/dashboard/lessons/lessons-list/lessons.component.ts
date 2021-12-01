75b50d5a6c049d2d3c53447e1a951864f18b1b78
Muhammad Sarfraz <replysarfraz@gmail.com>
2021-12-02 00:12:06 +0500

    Feat: Dashboard Active Sessions UI (complete).

--- src/app/dashboard/lessons/lessons-list/lessons.component.ts
+++ src/app/dashboard/lessons/lessons-list/lessons.component.ts
@@ -16,12 +16,7 @@ export interface PeriodicElement {
   endDate: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  // {title: 'Pitch Perfect', host: 'Matt Parson', participants: 3, startDate: 'Jan 12, 2021', endDate: 'Jan 22, 2021'},
-  // {title: 'Negotiations ', host: 'Matt Parson', participants: 3, startDate: 'Jan 12, 2021', endDate: 'Jan 22, 2021'},
-  // {title: 'The art of saying no', host: 'Matt Parson', participants: 3, startDate: 'Jan 12, 2021', endDate: 'Jan 22, 2021'},
-  // {title: 'How to do more with less', host: 'Matt Parson', participants: 3, startDate: 'Jan 12, 2021', endDate: 'Jan 22, 2021'},
-];
+const ELEMENT_DATA: PeriodicElement[] = [];
 
 @Component({
   selector: 'benji-lessons-list',
@@ -34,8 +29,27 @@ export class LessonsComponent implements OnInit {
 
   eventsSubject: Subject<void> = new Subject<void>();
 
-  displayedColumns: string[] = ['title', 'host', 'participants', 'startDate', 'endDate'];
+  displayedColumns: string[] = ['title', 'host', 'participants', 'startDate', 'endDate', 'options'];
   dataSource = ELEMENT_DATA;
+
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+  options = [
+    {
+      option: 'Open',
+      icon: '../../../../assets/img/dashboard-active-sessions/open.svg'
+    },
+    {
+      option: 'View only',
+      icon: '../../../../assets/img/dashboard-active-sessions/view-only.svg'
+    },
+    {
+      option: 'Closed',
+      icon: '../../../../assets/img/dashboard-active-sessions/closed.svg'
+    },
+  ]
+
+  selectedCategory = 'Open';
 
   edit(lesson, $event) {
     if (!this.isTemplates) {
@@ -56,23 +70,21 @@ export class LessonsComponent implements OnInit {
       this.lessons = orderBy(this.lessons, (lesson) => new Date(lesson.last_edited), 'desc');
     }
 
-    console.log(this.lessonRuns );
-
-    this.lessonRuns.forEach((val: any) => {
+    const slicedArray = this.lessonRuns.slice(0, 5);
+    slicedArray.forEach((val: any) => {
+      let startDate = new Date(val.start_time);
+      let endDate = new Date(val.end_time);
       this.dataSource.push(
         { title: val.lesson.lesson_name, 
           host:  val.host.first_name + ' ' + val.host.last_name, 
           participants: val.participant_set.length, 
-          startDate: val.start_time, 
-          endDate: val.end_time
+          startDate: this.months[startDate.getMonth()] +' '+ startDate.getDate().toString() +', '+ startDate.getFullYear(),
+          endDate: this.months[endDate.getMonth()] +' '+ endDate.getDate().toString() +', '+ endDate.getFullYear(),
         })
     });
   }
 
   openDetails(lesson: Lesson) {
-    // this.router.navigate(['lesson', lesson.id], {
-    //   relativeTo: this.activatedRoute,
-    // });
     this.adminService.getLessonDetails(lesson.id).subscribe((res: Lesson) => {
       if (res.lesson_details) {
         this.contextService.lesson = res;
