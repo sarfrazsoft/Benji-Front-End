e9daa8e714c135fe3bc10a633b2d4e807cb210f7
Muhammad Sarfraz <replysarfraz@gmail.com>
2021-12-01 07:08:27 +0500

    Feat: Dashboard Active Sessions UI (about to complete).

--- src/app/dashboard/lessons/lessons-list/lessons.component.ts
+++ src/app/dashboard/lessons/lessons-list/lessons.component.ts
@@ -9,23 +9,18 @@ import * as moment from 'moment';
 import { Subject } from 'rxjs/Subject';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  title: string;
+  host: string;
+  participants: number;
+  startDate: string;
+  endDate: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  // {title: 'Pitch Perfect', host: 'Matt Parson', participants: 3, startDate: 'Jan 12, 2021', endDate: 'Jan 22, 2021'},
+  // {title: 'Negotiations ', host: 'Matt Parson', participants: 3, startDate: 'Jan 12, 2021', endDate: 'Jan 22, 2021'},
+  // {title: 'The art of saying no', host: 'Matt Parson', participants: 3, startDate: 'Jan 12, 2021', endDate: 'Jan 22, 2021'},
+  // {title: 'How to do more with less', host: 'Matt Parson', participants: 3, startDate: 'Jan 12, 2021', endDate: 'Jan 22, 2021'},
 ];
 
 @Component({
@@ -39,7 +34,7 @@ export class LessonsComponent implements OnInit {
 
   eventsSubject: Subject<void> = new Subject<void>();
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['title', 'host', 'participants', 'startDate', 'endDate'];
   dataSource = ELEMENT_DATA;
 
   edit(lesson, $event) {
@@ -61,7 +56,17 @@ export class LessonsComponent implements OnInit {
       this.lessons = orderBy(this.lessons, (lesson) => new Date(lesson.last_edited), 'desc');
     }
 
-    console.log(this.lessonRuns);
+    console.log(this.lessonRuns );
+
+    this.lessonRuns.forEach((val: any) => {
+      this.dataSource.push(
+        { title: val.lesson.lesson_name, 
+          host:  val.host.first_name + ' ' + val.host.last_name, 
+          participants: val.participant_set.length, 
+          startDate: val.start_time, 
+          endDate: val.end_time
+        })
+    });
   }
 
   openDetails(lesson: Lesson) {
