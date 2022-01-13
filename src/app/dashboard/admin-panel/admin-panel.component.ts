8a6183304524505a8e32981dea4f568f9d32c247
Muhammad Sarfraz <replysarfraz@gmail.com>
2022-01-13 18:54:39 +0500

    Feat: Dashboard filter drop down for sessions types.

--- src/app/dashboard/admin-panel/admin-panel.component.ts
+++ src/app/dashboard/admin-panel/admin-panel.component.ts
@@ -55,20 +55,6 @@ export class AdminPanelComponent implements OnInit {
       this.lessons = data.dashData.lessons.filter((lesson) => lesson.public_permission !== 'duplicate');
 
       this.lessonRuns = data.dashData.lessonRuns;
-      // if (!data.dashData.user.job_title) {
-      //   this.dialog
-      //     .open(JobInfoDialogComponent, {
-      //       data: {
-      //         name: data.dashData.user.first_name
-      //       },
-      //       disableClose: true,
-      //       panelClass: 'dashboard-dialog'
-      //     })
-      //     .afterClosed()
-      //     .subscribe(res => {
-      //       console.log(res);
-      //     });
-      // }
     });
   }
 
@@ -93,40 +79,3 @@ export class AdminPanelComponent implements OnInit {
   }
 
 }
-
-// {
-//   "titleactivity": {
-//     "id": 1253,
-//     "next_activity_start_timer": {
-//       "id": 549,
-//       "status": "running",
-//       "start_time": "2021-03-08T13:04:26.229653-05:00",
-//       "end_time": "2021-03-08T15:51:06.229653-05:00",
-//       "total_seconds": 10000,
-//       "remaining_seconds": 9999.859045
-//     },
-//     "facilitation_status": "running",
-//     "auto_next": true,
-//     "next_activity_delay_seconds": 10000,
-//     "title_image": "emoji://1F642",
-//     "main_title": "mainscreen heading text",
-//     "title_text": "mainscreen detailed text",
-//     "cards": [
-//       {
-//         "title_image": "emoji://1F642",
-//         "main_title": "mainscreen heading text",
-//         "title_text": "mainscreen detailed text"
-//       }
-//     ],
-//     "hide_timer": true,
-//     "activity_id": "1612207589964",
-//     "description": null,
-//     "start_time": "2021-03-08T13:04:26.225409-05:00",
-//     "end_time": null,
-//     "is_paused": false,
-//     "run_number": 0,
-//     "polymorphic_ctype": 46,
-//     "next_activity": 1254,
-//     "activity_type": "TitleActivity"
-//   }
-// }
