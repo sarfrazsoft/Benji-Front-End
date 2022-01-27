29d1841778994f1bc68cd632dbc6a174206fd840
Muhammad Sarfraz <replysarfraz@gmail.com>
2022-01-27 07:04:39 +0500

    Feat: 3.Misc Fixes QA Jan 25th Test.

--- src/app/pages/lesson/main-screen/brainstorming-activity/brainstorm-card/brainstorm-card.component.ts
+++ src/app/pages/lesson/main-screen/brainstorming-activity/brainstorm-card/brainstorm-card.component.ts
@@ -206,14 +206,14 @@ export class BrainstormCardComponent implements OnInit, OnChanges {
   }
 
   showDetailedIdea(idea: Idea) {
-    // if (this.deviceService.isMobile()) {
-    //   this.openDialog(idea, "idea-detailed-mobile-dialog", false);
-    // } else {
-    //   this.openDialog(idea, "idea-detailed-dialog", true);
-    // }
+    if (this.deviceService.isMobile()) {
+      this.openDialog(idea, "idea-detailed-mobile-dialog", false);
+    } else {
+      this.openDialog(idea, "idea-detailed-dialog", true);
+    }
 
     // this.openDialog(idea, 'idea-detailed-mobile-dialog', false);
-    this.openDialog(idea, 'idea-detailed-dialog', true);
+    // this.openDialog(idea, 'idea-detailed-dialog', true);
   }
 
   openDialog(idea: Idea, assignedClass, isDesktop) {
