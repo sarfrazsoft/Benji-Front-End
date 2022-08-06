67bfba8114662bc2674cbb96f2f64ec2c36c677e
Muhammad Sarfraz <replysarfraz@gmail.com>
2022-08-06 00:10:07 +0000

    Merged in BEN-587-sidebar-design (pull request #120)
    BEN-587 sidebar design
    
    * Feat: Navigation moved to left.
    
    * Feat: Sidebar design first draft design complete.
    
    * Feat: Staging merged.
    
    * Feat: Sidebar modes different for settings and boards menus.
    
    * merge
    
    
    Approved-by: Muhammad Mahin Khan

--- src/app/pages/lesson/main-screen/main-screen-lesson.component.ts
+++ src/app/pages/lesson/main-screen/main-screen-lesson.component.ts
@@ -1,5 +1,5 @@
 import { viewClassName } from '@angular/compiler';
-import { AfterViewInit, ChangeDetectorRef, Component, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, HostListener, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSidenav } from '@angular/material/sidenav';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -25,7 +25,7 @@ import { BaseLessonComponent } from '../shared/base-lesson.component';
   selector: 'benji-main-screen-lesson',
   templateUrl: './main-screen-lesson.component.html',
 })
-export class MainScreenLessonComponent extends BaseLessonComponent implements OnInit {
+export class MainScreenLessonComponent extends BaseLessonComponent implements AfterViewInit {
   dialogRef: any;
 
   // board-menu variables
@@ -35,6 +35,8 @@ export class MainScreenLessonComponent extends BaseLessonComponent implements On
   @ViewChild(MainScreenToolbarComponent) msToolbar: MainScreenToolbarComponent;
   navType: string;
   sideNavPosition: 'start' | 'end';
+  sideNavMode: 'side' | 'over';
+  public innerWidth: any;
 
   constructor(
     protected deviceDetectorService: DeviceDetectorService,
@@ -63,14 +65,6 @@ export class MainScreenLessonComponent extends BaseLessonComponent implements On
       ref,
       matSnackBar
     );
-
-    // groupingToolService.showGroupingToolMainScreen$.subscribe((val) => {
-    //   this.showGroupingOnScreen = val;
-    //   console.log(val);
-    //   if (val) {
-    //     this.openParticipantGroupingToolDialog();
-    //   }
-    // });
   }
 
   at: typeof ActivityTypes = ActivityTypes;
@@ -90,6 +84,18 @@ export class MainScreenLessonComponent extends BaseLessonComponent implements On
     this.at.montyHall,
   ];
 
+  ngAfterViewInit() {
+    this.innerWidth = window.innerWidth;
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = event.target.innerWidth; 
+    this.innerWidth < 848 && this.navType === 'boards' ? 
+      this.sideNavMode = 'over' : 
+      this.sideNavMode = 'side'; 
+  }
+
   isPaused() {
     const activity_type = this.serverMessage.activity_type.toLowerCase();
     return this.serverMessage[activity_type].is_paused;
@@ -99,12 +105,15 @@ export class MainScreenLessonComponent extends BaseLessonComponent implements On
     if (type === 'board-settings') {
       this.navType = 'board-settings';
       this.sideNavPosition = 'end';
+      this.sideNavMode = 'over';
     } else if (type === 'boards') {
       this.navType = 'boards';
       this.sideNavPosition = 'start';
-      //this.sideNavPosition = 'end';
+      this.innerWidth < 848 ?
+        this.sideNavMode = 'over' :
+        this.sideNavMode = 'side';
     }
-    type ? this.sidenav.open() : this.sidenav.close();
+    type ? this.sidenav.open() : this.close();
   }
 
   openBoardSettings() {
@@ -113,5 +122,8 @@ export class MainScreenLessonComponent extends BaseLessonComponent implements On
 
   close() {
     this.sidenav.close();
+    this.sideNavMode = null;
+    this.navType = null;
+    this.sideNavPosition = null;
   }
 }
