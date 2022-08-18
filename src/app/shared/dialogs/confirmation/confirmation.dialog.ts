c539245840d17bf56ea0bfa66bf546713cfa78b5
mahin@mybenji.com <tutenstine@gmail.com>
2022-08-17 20:20:58 -0400

    removed button

--- src/app/shared/dialogs/confirmation/confirmation.dialog.ts
+++ src/app/shared/dialogs/confirmation/confirmation.dialog.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export interface ConfirmationDialogData {
   confirmationTitle?: string;
@@ -16,8 +16,7 @@ export class ConfirmationDialogComponent implements OnInit {
   public confirmationTitle: string;
   public confirmationMessage: string;
   public actionButton: string;
-  public actionButton: string;
-  public cancelButton2: string;
+  public cancelButton: string;
 
   constructor(
     private dialogRef: MatDialogRef<ConfirmationDialogComponent>,
